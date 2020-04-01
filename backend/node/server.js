9d3b6ce92dc8431ca914a33474a10e2788e938e4
Djaenk <Djaenk@users.noreply.github.com>
2020-03-31 20:51:39 -0500

    specialized backend for our database server

--- backend/node/server.js
+++ backend/node/server.js
@@ -6,11 +6,11 @@ const { log, ExpressAPILogMiddleware } = require('@rama41222/node-logger');
 
 //mysql connection
 var connection = mysql.createConnection({
-  host: 'backend-db',
+  host: '10.59.43.164',
   port: '3306',
-  user: 'manager',
-  password: 'Password',
-  database: 'db'
+  user: 'dbgui',
+  password: 'dbgui',
+  database: 'pharmtech'
 });
 
 //set up some configs for express.
@@ -42,52 +42,6 @@ connection.connect(function (err) {
 //GET /
 app.get('/', (req, res) => {
   res.status(200).send('Go to 0.0.0.0:3000.');
-});
-
-
-//POST /reset
-app.post('/reset', (req, res) => {
-  connection.query('drop table if exists test_table', function (err, rows, fields) {
-    if (err)
-      logger.error("Can't drop table");
-  });
-  connection.query('CREATE TABLE `db`.`test_table` (`id` INT NOT NULL AUTO_INCREMENT, `value` VARCHAR(45), PRIMARY KEY (`id`), UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE);', function (err, rows, fields) {
-    if (err)
-      logger.error("Problem creating the table test_table");
-  });
-  res.status(200).send('created the table');
-});
-
-//POST /multplynumber
-app.post('/multplynumber', (req, res) => {
-  console.log(req.body.product);
-
-  connection.query('INSERT INTO `db`.`test_table` (`value`) VALUES(\'' + req.body.product + '\')', function (err, rows, fields) {
-    if (err){
-      logger.error("Problem inserting into test table");
-    }
-    else {
-      res.status(200).send(`added ${req.body.product} to the table!`);
-    }
-  });
-});
-
-//GET /checkdb
-app.get('/values', (req, res) => {
-  connection.query('SELECT value FROM `db`.`test_table`', function (err, rows, fields) {
-    if (err) {
-      logger.error("Error while executing Query");
-      res.status(400).json({
-        "data": [],
-        "error": "MySQL error"
-      })
-    }
-    else{
-      res.status(200).json({
-        "data": rows
-      });
-    }
-  });
 });
 
 //connecting the express object to listen on a particular port as defined in the config object.
